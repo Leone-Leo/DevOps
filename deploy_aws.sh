f4fdf43691323da48676366d58aa0e5bb192e692
Leone-Leo <122890809+Leone-Leo@users.noreply.github.com>
2023-06-23 17:26:58 -0300

    Create deploy_aws.sh

---
+++ deploy_aws.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script para implantar em um ambiente AWS
+
+echo "Iniciando a implantação no ambiente AWS..."
+
+# Comandos para criar ou atualizar recursos na AWS usando a AWS CLI ou o SDK
+
+echo "Implantação no ambiente AWS concluída com sucesso!"

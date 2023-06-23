8f80673947f14496df72203984a8e95800ecc05d
Leone-Leo <122890809+Leone-Leo@users.noreply.github.com>
2023-06-23 17:29:55 -0300

    Create automação_testes.sh

---
+++ automação_testes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script de automação de testes
+
+echo "Iniciando a automação de testes..."
+
+# Comandos para executar os testes automatizados usando uma ferramenta específica
+# Exemplo: npm test, pytest, etc.
+
+echo "Automação de testes concluída com sucesso!"
